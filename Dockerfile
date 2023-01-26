328203a78487c11a4083379307e0f1c8abb89405
Palleri <palleri@mario.se>
2023-01-26 12:53:55 +0100

    Created cronjob

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,15 @@ FROM php:8.1-rc-apache
 
 RUN curl -L "https://github.com/docker/compose/releases/download/2.15.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-
+RUN mkdir /app
 RUN apt update && apt install docker.io -y
 RUN chmod +x /usr/local/bin/docker-compose
+RUN curl -L "https://raw.githubusercontent.com/mag37/dockcheck/main/dockcheck.sh" -o /app/dockcheck.sh
+RUN curl -L "https://github.com/regclient/regclient/releases/download/v0.4.5/regctl-linux-amd64" -o /app/regctl
 
-
-RUN mkdir /app
 COPY run.sh /app/run.sh 
 
-#COPY bin/* /etc/
+COPY dockcheck /etc/cron.daily/dockcheck
 COPY src/ /var/www/
 
 
--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-curl -L "https://raw.githubusercontent.com/mag37/dockcheck/main/dockcheck.sh" > /app/dockcheck.sh
+#curl -L "https://raw.githubusercontent.com/mag37/dockcheck/main/dockcheck.sh" > /app/dockcheck.sh
 chmod +x /app/dockcheck.sh
 cp /var/www/* /var/www/html/
 
-curl -L "https://github.com/regclient/regclient/releases/download/v0.4.5/regctl-linux-amd64" > /app/regctl
+#curl -L "https://github.com/regclient/regclient/releases/download/v0.4.5/regctl-linux-amd64" > /app/regctl
 chmod +x /app/regctl
+chmod +x /etc/cron.daily/dockcheck
 mv /app/regctl /usr/bin/
 /app/dockcheck.sh | sed -r "s:\x1B\[[0-9;]*[mK]::g" > /app/containers
 
