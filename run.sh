b0f9e57d0263c64a9f05a6d2865857c3f5f3610d
Palleri <Skoog.timmy@gmail.com>
2023-01-28 14:36:06 +0100

    bug fix mv

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ chmod +x /app/dockcheck.sh
 cp /var/www/tmp/* /var/www/html/
 
 rm -rf /etc/cron.daily/*
-mv /tmp/dockcheck /etc/cron.daily/dockcheck
+cp /tmp/dockcheck /etc/cron.daily/dockcheck
 chmod +x /app/regctl
 chmod +x /etc/cron.daily/dockcheck
-mv /app/regctl /usr/bin/
+cp /app/regctl /usr/bin/
 /app/dockcheck.sh | sed -r "s:\x1B\[[0-9;]*[mK]::g" > /app/containers
 apache2-foreground
