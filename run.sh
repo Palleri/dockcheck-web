4d783d811a88c99fc79c4ff287001cc963398b8f
Palleri <Skoog.timmy@gmail.com>
2023-01-29 09:06:19 +0100

    edit cronjob

--- run.sh
+++ run.sh
@@ -8,4 +8,5 @@ chmod +x /app/regctl
 chmod +x /etc/cron.daily/dockcheck
 cp /app/regctl /usr/bin/
 /app/dockcheck.sh | sed -r "s:\x1B\[[0-9;]*[mK]::g" > /app/containers
+service cron start
 apache2-foreground
