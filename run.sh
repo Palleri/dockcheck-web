2c87c1ff988fa6a0f93e9a9efa4fc93be476bcb5
Palleri <Skoog.timmy@gmail.com>
2023-01-31 19:51:17 +0100

    Added watcher

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ echo "# Checking for new updates #"
 echo "# This might take a while, it depends on how many containers are running #"
 chmod +x /app/dockcheck.sh
 cp /var/www/tmp/* /var/www/html/
+touch /var/www/html/update.txt
+chown www-data:www-data /var/www/html/update.txt
 
 rm -rf /etc/cron.daily/*
 cp /tmp/dockcheck /etc/cron.daily/dockcheck
@@ -13,4 +15,5 @@ cp /app/regctl /usr/bin/
 /app/dockcheck.sh -n | sed -r "s:\x1B\[[0-9;]*[mK]::g" > /app/containers_temp
 cat /app/containers_temp > /app/containers
 service cron start
+/var/www/html/watcher.sh </dev/null >/dev/null 2>&1 & disown
 apache2-foreground
